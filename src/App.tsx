b485150aad5475911c4a40c02bb72d8af92502de
JerryShum <jerryshum2636@gmail.com>
2024-02-26 10:23:26 -0500

    feat: changed to vertical animation, used useGSAP hook, (cleaner)

--- src/App.tsx
+++ src/App.tsx
@@ -7,57 +7,49 @@ function App() {
   //Creates a reference for the root level element (used for scoping)
 
   // uselayouteffect triggers after all the dom mutations happens
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
+  useGSAP(
+    () => {
       const t1 = gsap.timeline();
-      t1.from("#intro-slider", {
-        xPercent: "-100",
-        duration: 1.3,
+      t1.from(["#title-1, #title-2, #title-3"], {
+        opacity: 0,
+        y: "+=30",
+        stagger: 0.5,
+      });
+      t1.to(["#title-1, #title-2, #title-3"], {
+        opacity: 0,
+        y: "-=30",
         delay: 0.3,
-      })
-        .from(["#title-1, #title-2, #title-3"], {
-          opacity: 0,
-          y: "+=30",
-          stagger: 0.5,
-        })
-        .to(["#title-1, #title-2, #title-3"], {
-          opacity: 0,
-          y: "-=30",
-          delay: 0.3,
-          stagger: 0.5,
-        })
-        .to("#intro-slider", {
-          xPercent: "-100",
-          duration: 1.3,
-        })
-        .from("#welcome", {
-          opacity: 0,
-          duration: 1.5,
-        });
-    }, comp);
+        stagger: 0.3,
+      });
+      t1.to("#intro-slider", {
+        yPercent: "-100",
+        duration: 0.7,
+      });
 
-    return () => ctx.revert();
-  }, []);
+      t1.from("#welcome", {
+        opacity: 0,
+        duration: 1.5,
+      });
+    },
+    { scope: comp }
+  );
 
   return (
     <div className="relative" ref={comp}>
       <div
         id="intro-slider"
-        className="h-screen w-screen p-10 bg-gray-50 text-gray-700 absolute top-0 left-0 font-spaceGrotesk z-10 flex flex-col gap-10 tracking-tight"
+        className="h-screen w-screen p-10 bg-gray-900 text-gray-200 absolute top-0 left-0 font-spaceGrotesk z-10 flex flex-col gap-10 tracking-tight items-center justify-center"
       >
-        <h1 className="text-9xl" id="title-1">
-          Software Engineer
-        </h1>
         <h1 className="text-9xl" id="title-2">
-          Designer
+          Jerry Shum
         </h1>
         <h1 className="text-9xl" id="title-3">
-          Student
+          Portfolio
         </h1>
       </div>
-      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+      <div className="h-screen flex bg-gray-50 justify-center place-items-center">
         <h1
-          className="text-9xl font-bold text-gray-400 font-spaceGrotesk"
+          className="text-9xl font-bold text-gray-900 font-spaceGrotesk"
           id="welcome"
         >
           Hello World!
