e40e33054e069c952000f2d79aad284740f9a13a
JerryShum <jerryshum2636@gmail.com>
2024-02-27 13:16:48 -0500

    feat: added timer and state to properly time intro animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import Intro from "./components/Intro";
 import Hero from "./components/Hero";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [introDone, setIntroDone] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIntroDone(true);
+    }, 3500); // 2000ms = 5s
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <Hero />
-    </div>
+    <div>{introDone ? <Hero /> : <Intro />}</div>
+    // if introDone = true, return Hero else return Intro
   );
 }
 
