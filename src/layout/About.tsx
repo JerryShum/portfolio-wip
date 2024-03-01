653b916ddf6e8e9f7697b314e4eaf62e7388c9f1
JerryShum <jerryshum2636@gmail.com>
2024-03-01 11:35:23 -0500

    feat: cleanup and imported compressed image

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -38,8 +38,10 @@ function About() {
       <h1 className=" uppercase font-league-spartan font-semibold text-9xl mt-20">
         Who Am I?
       </h1>
-
-      <img src={jerryImage} className="w-1/4" />
+      <img
+        src={jerryImage}
+        className="absolute top-1/2 left-1/2 w-1/3 border rounded-lg border-black "
+      />
     </div>
   );
 }
