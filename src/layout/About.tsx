9b119c967e817a157b07b20d9a3a55dac986ec14
JerryShum <jerryshum2636@gmail.com>
2024-03-02 10:25:29 -0500

    feat: imported jerryGame image to about section

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -2,17 +2,22 @@ import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import jerryImage from "../assets/jerry_oec.webp";
+import jerryOEC from "../assets/jerry_oec.webp";
+import jerryGame from "../assets/Jerry_Game.webp";
 
 function About() {
   return (
-    <div className="relative h-screen w-full px-20 ">
-      <h1 className=" uppercase font-league-spartan font-semibold text-9xl mt-20">
+    <div className="relative h-screen w-full p-20 ">
+      <h1 className=" uppercase font-league-spartan font-semibold text-9xl">
         Who Am I?
       </h1>
       <img
-        src={jerryImage}
-        className="absolute top-1/2 left-1/2 w-1/3 border rounded-lg border-black "
+        src={jerryOEC}
+        className="absolute top-1/2 left-1/2 w-1/3 border rounded-[30px] border-red-500"
+      />
+      <img
+        src={jerryGame}
+        className="absolute top-1/2 left-1/2 w-1/3 border rounded-[30px] border-red-500"
       />
     </div>
   );
