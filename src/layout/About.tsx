f43e27e69d321413952229d34c890936908daa74
JerryShum <jerryshum2636@gmail.com>
2024-03-04 01:54:18 -0500

    feat: removed lazy loading component

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import JerryOEC from "../assets/jerry_oec_resized.jpeg";
 import JerryGame from "../assets/Jerry_Game_resized.jpeg";
@@ -9,24 +8,38 @@ import JerryOECPlaceholder from "../assets/Jerry_OEC_Placeholder.jpeg";
 import JerryGamePlaceholder from "../assets/Jerry_Game_Placeholder.jpeg";
 
 function About() {
+  const imageRef1 = useRef(null);
+  const imageRef2 = useRef(null);
+
+  // gsap.registerPlugin(ScrollTrigger);
+
+  // useGSAP(() => {
+  //   const image = imageRef.current;
+  // }, []);
+
   return (
     <div className="relative h-screen w-full">
-      <h1 className=" uppercase font-league-spartan font-semibold text-9xl">
-        Who Am I?
-      </h1>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="uppercase font-league-spartan font-semibold text-9xl ">
+          Who Am I?
+        </h1>
+        <span>Hello World!</span>
+      </div>
 
-      <div className="relative">
-        <LazyLoadImage
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <img
           src={JerryOEC}
-          className="absolute h-auto w-1/3 top-6/12 left-0 right-0  border rounded-[30px] border-red-500"
-          id="#sliding-image"
-          placeholderSrc={JerryOECPlaceholder}
+          className="h-[800px] w-[640px] object-cover rounded-[30px] border border-red-500 z-10"
+          id="#sliding-image1"
+          ref={imageRef1}
+          // placeholderSrc={JerryOECPlaceholder}
         />
-        <LazyLoadImage
+        <img
           src={JerryGame}
-          className="absolute h-auto w-1/3 top-6/12 left-0 right-0 border rounded-[30px] border-red-500"
-          id="#sliding-image"
-          placeholderSrc={JerryGamePlaceholder}
+          className="h-[800] w-[640] object-cover rounded-[30px] border border-red-500 absolute top-0 left-0 z-20"
+          id="#sliding-image2"
+          ref={imageRef2}
+          // placeholderSrc={JerryGamePlaceholder}
         />
       </div>
     </div>
