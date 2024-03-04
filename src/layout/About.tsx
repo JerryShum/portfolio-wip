62de969caf9799baf24bc9efce98e0b24fed4d3f
JerryShum <jerryshum2636@gmail.com>
2024-03-04 02:19:57 -0500

    feat: improving code structure

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -41,27 +41,26 @@ function About() {
   }, []);
 
   return (
-    <div className="relative h-screen w-full" id="section-start">
-      <div className="h-screen flex flex-col justify-center items-center">
+    <div className=" w-full" id="section-start">
+      <div className="relative h-screen flex flex-col justify-center items-center">
         <h1 className="uppercase font-league-spartan font-semibold text-9xl ">
           Who Am I?
         </h1>
         <span>Hello World!</span>
-      </div>
-
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <img
-          src={JerryOEC}
-          className="h-[800px] w-[640px] rotate-2 object-cover rounded-[30px] border border-red-500 z-10"
-          id="#sliding-image"
-          ref={imageRef1}
-        />
-        <img
-          src={JerryGame}
-          className="h-[800] w-[640] -rotate-2 object-cover rounded-[30px] border border-red-500 absolute top-0 left-0 z-20"
-          id="#sliding-image"
-          ref={imageRef2}
-        />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <img
+            src={JerryOEC}
+            className="h-[800px] w-[640px] rotate-2 object-cover rounded-[30px] border border-red-500 z-10"
+            id="#sliding-image"
+            ref={imageRef1}
+          />
+          <img
+            src={JerryGame}
+            className="h-[800] w-[640] -rotate-2 object-cover rounded-[30px] border border-red-500 absolute top-0 left-0 z-20"
+            id="#sliding-image"
+            ref={imageRef2}
+          />
+        </div>
       </div>
     </div>
   );
